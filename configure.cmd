6bc75ac8670690e754ee42e953f5772ed4973d84
KO Myung-Hun <komh@chollian.net>
2017-01-24 16:47:00 +0900

    my: disable XMM feature
    Compiling libmp3lame/vector/xmm_quantize_sub.c on gcc v4.8+ fails.
    This is due to regression of gcc v4.8+.
    
    Disable correspondent features on gcc v4.8+
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -1,3 +1,15 @@
 extproc sh
 
-./configure. --enable-shared=no --enable-static --enable-nasm "$@"
+gccver=$(gcc -dumpversion)
+gccver_major=${gccver%%.*}
+gccver_minor=${gccver#*.}
+gccver_minor=${gccver_minor%.*}
+
+ac_cv_vars=
+if test $gccver_major -eq 4 -a $gccver_minor -ge 8; then
+  # gcc v4.8+ has a regression. XMM compilation fails. Disable it
+  # gcc 5.x fixed this
+  ac_cv_vars="$ac_cv_vars ac_cv_header_xmmintrin_h=no"
+fi
+
+./configure. --enable-shared=no --enable-static --enable-nasm $ac_cv_vars "$@"
